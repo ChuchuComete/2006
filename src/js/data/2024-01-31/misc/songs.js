0b53fd9c79dc5759f938525448736e655be34887
ChuchuComete <160599605+ChuchuComete@users.noreply.github.com>
2024-10-10 22:05:53 +0200

    Update songs.js

--- src/js/data/2024-01-31/misc/songs.js
+++ src/js/data/2024-01-31/misc/songs.js
@@ -1,8 +1,8 @@
 
 export const songs = [
 	{
-    name: "Lupin the Third: Mine Fujiko to Iu Onna - Duty Friend by NIKIIE",
-    videoId : "https://nl.catbox.video/ty9egp.webm",
+    name: "Aa Megami-sama: Sorezore no Tsubasa - Shiawase no Iro by Yoko Ishida",
+    videoId : "https://naedist.animemusicquiz.com/2rnqcn.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -11,8 +11,8 @@ export const songs = [
     }
 },
 	{
-    name: "Hai to Gensou no Grimgar - Harvest by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/hg5csx.webm",
+    name: "Air Gear - Chain by BACK-ON",
+    videoId : "https://naedist.animemusicquiz.com/o6drw7.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -21,8 +21,8 @@ export const songs = [
     }
 },
 	{
-    name: "Hai to Gensou no Grimgar - Cultivate by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/qy8p8r.webm",
+    name: "Angel Heart1 - Battlefield of Love by Asami Izawa",
+    videoId : "https://naedist.animemusicquiz.com/4esyds.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -31,8 +31,8 @@ export const songs = [
     }
 },
 	{
-    name: "Hai to Gensou no Grimgar - rainy tone by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/rpk4fm.webm",
+    name: "Angel Heart2 - Serenade by Kanon",
+    videoId : "https://naedist.animemusicquiz.com/yo7d4k.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -41,8 +41,8 @@ export const songs = [
     }
 },
 	{
-    name: "Hai to Gensou no Grimgar - seeds by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/i56l1p.webm",
+    name: "Aria the Natural - Euphoria by Yui Makino",
+    videoId : "https://naedist.animemusicquiz.com/qmvv09.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -51,8 +51,8 @@ export const songs = [
     }
 },
 	{
-    name: "Hai to Gensou no Grimgar - sun will rise by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/4jj1fj.webm",
+    name: "Baldr Force EXE Resolution - Face of Fact -RESOLUTION ver.- by KOTOKO",
+    videoId : "https://naedist.animemusicquiz.com/r9r0ex.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -61,8 +61,8 @@ export const songs = [
     }
 },
 	{
-    name: "Sakura Quest - Morning Glory by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/rr0rdm.webm",
+    name: "Black Lagoon - Red fraction by MELL",
+    videoId : "https://naedist.animemusicquiz.com/yqgyqh.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -71,8 +71,8 @@ export const songs = [
     }
 },
 	{
-    name: "Sakura Quest - Baby's breath by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/odxzn2.webm",
+    name: "Bleach1 - HANABI by Ikimono-gakari",
+    videoId : "https://naedist.animemusicquiz.com/hetg5s.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -81,8 +81,8 @@ export const songs = [
     }
 },
 	{
-    name: "Sakura Quest - thyme by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/2fm1wi.webm",
+    name: "Bleach2 - MOVIN!! by Takacha",
+    videoId : "https://naedist.animemusicquiz.com/yl6i8b.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -91,8 +91,8 @@ export const songs = [
     }
 },
 	{
-    name: "Fairy Gone - Ash-like Snow by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/90ezsl.webm",
+    name: "Blood+1 - This Love by Angela Aki",
+    videoId : "https://naedist.animemusicquiz.com/3945v5.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -101,8 +101,8 @@ export const songs = [
     }
 },
 	{
-    name: "Fairy Gone - Danse Dance by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/1gbwgr.webm",
+    name: "Blood+2 - Colors of the Heart by UVERworld",
+    videoId : "https://naedist.animemusicquiz.com/stv0c1.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -111,8 +111,8 @@ export const songs = [
     }
 },
 	{
-    name: "Fairy Gone - Forest Gleam by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/27rmfq.webm",
+    name: "Bokura ga Ita - Sunset -album version- by Mi",
+    videoId : "https://naedist.animemusicquiz.com/yd86s7.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -121,8 +121,8 @@ export const songs = [
     }
 },
 	{
-    name: "Fairy Gone - blowball by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/kdxr1l.webm",
+    name: "Busou Renkin - Makka na Chikai by Fukuyama Yoshiki",
+    videoId : "https://naedist.animemusicquiz.com/idc2sp.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -131,8 +131,8 @@ export const songs = [
     }
 },
 	{
-    name: "Fairy Gone - Hertzsprung gap by (K)NoW_NAME:Ayaka Tachibana & NIKIIE",
-    videoId : "https://nl.catbox.video/yqwruc.webm",
+    name: "Code Geass: Hangyaku no Lelouch - COLORS by FLOW",
+    videoId : "https://naedist.animemusicquiz.com/w9hgpy.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -141,8 +141,8 @@ export const songs = [
     }
 },
 	{
-    name: "Fairy Gone - STILL STANDING by (K)NoW_NAME:Ayaka Tachibana & NIKIIE & AIJ",
-    videoId : "https://nl.catbox.video/b48abm.webm",
+    name: "D.Gray-man - INNOCENT SORROW by abingdon boys school",
+    videoId : "https://naedist.animemusicquiz.com/6cpvug.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -151,8 +151,8 @@ export const songs = [
     }
 },
 	{
-    name: "Fairy Gone - Red dog chases by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/4vv2lx.webm",
+    name: "Eiga Doraemon: Nobita no Kyouryuu 2006 - Boku Note by Sukima Switch",
+    videoId : "https://naedist.animemusicquiz.com/nq6jak.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -161,8 +161,8 @@ export const songs = [
     }
 },
 	{
-    name: "Fairy Gone - Everlast... by (K)NoW_NAME:NIKIIE & AIJ",
-    videoId : "https://nl.catbox.video/uq2nsj.webm",
+    name: "Ergo Proxy - Paranoid Android by Radiohead",
+    videoId : "https://naedist.animemusicquiz.com/096i32.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -171,8 +171,8 @@ export const songs = [
     }
 },
 	{
-    name: "Dorohedoro - Who am I? by (K)NoW_NAME:Ayaka Tachibana & NIKIIE",
-    videoId : "https://nl.catbox.video/i7swn3.webm",
+    name: "Fate/stay night1 - Kirameku Namida wa Hoshi ni by Sachi Tainaka",
+    videoId : "https://naedist.animemusicquiz.com/d1boaz.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -181,8 +181,8 @@ export const songs = [
     }
 },
 	{
-    name: "Dorohedoro - D.D.D.D. by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/timams.webm",
+    name: "Fate/stay night2 - Anata ga Ita Mori by Jyukai",
+    videoId : "https://naedist.animemusicquiz.com/0mr1fc.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -191,8 +191,8 @@ export const songs = [
     }
 },
 	{
-    name: "Dorohedoro - SECONDs FLY by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/i7edi3.webm",
+    name: "Flag - Lights by Eri Nobuchika",
+    videoId : "https://naedist.animemusicquiz.com/9sxmor.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -201,8 +201,8 @@ export const songs = [
     }
 },
 	{
-    name: "Dorohedoro - 404 by (K)NoW_NAME:NIKIIE & AIJ",
-    videoId : "https://nl.catbox.video/z9xssy.webm",
+    name: "Freedom - This Is Love by Hikaru Utada",
+    videoId : "https://naedist.animemusicquiz.com/fr5hh8.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -211,8 +211,8 @@ export const songs = [
     }
 },
 	{
-    name: "Spy × Family - Breeze by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/is51b0.webm",
+    name: "Gekijouban Harukanaru Toki no Naka de: Maihitoyo - Harari, Hirari by sona",
+    videoId : "https://naedist.animemusicquiz.com/crtf6f.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -221,8 +221,8 @@ export const songs = [
     }
 },
 	{
-    name: "Spy × Family - GOOD DAY by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/r8pmce.webm",
+    name: "Gin-iro no Kami no Agito - Ai no Melody by KOKIA",
+    videoId : "https://naedist.animemusicquiz.com/27p221.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -231,8 +231,8 @@ export const songs = [
     }
 },
 	{
-    name: "Spy × Family - Little steps by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/q1gwh4.webm",
+    name: "Ginga Tetsudou Monogatari: Eien e no Bunkiten - CARRY THE LIGHT by Jia Jia",
+    videoId : "https://vhdist1.catbox.video/mrn8d0.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -241,8 +241,288 @@ export const songs = [
     }
 },
 	{
-    name: "Spy × Family - Garden by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/2fv70p.webm",
+    name: "Gintama - Tooi Nioi by YO-KING",
+    videoId : "https://naedist.animemusicquiz.com/m815im.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Glass no Kamen (2005) - Hello Hello ~another star~ by CORE OF SOUL",
+    videoId : "https://naedist.animemusicquiz.com/wu2uwp.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "HANOKA - Dual Love on the planet ~Hanoka~ By Sakura Nogawa",
+    videoId : "https://naedist.animemusicquiz.com/1k8z72.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Higurashi no Naku Koro ni - Higurashi no Naku Koro ni by Eiko Shimamiya",
+    videoId : "https://naedist.animemusicquiz.com/3czpgu.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Jyu-Oh-Sei - Te o Tsunaide by Younha",
+    videoId : "https://naedist.animemusicquiz.com/gb6rtf.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Kemonozume - Auvers Blue by Katteni-Shiyagare",
+    videoId : "https://naedist.animemusicquiz.com/nsema7.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Kiba - Hakanaku Tsuyoku by Younha",
+    videoId : "https://naedist.animemusicquiz.com/4b5uh2.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Kidou Senshi Gundam Seed C.E. 73: Stargazer - STARGAZER ~ Hoshi no Tobira by Satori Negishi",
+    videoId : "https://naedist.animemusicquiz.com/t2yc5k.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Kyou no 5 no 2 - Yakusoku by Mai Kadowaki, Mamiko Noto, Mikako Takahashi",
+    videoId : "https://naedist.animemusicquiz.com/lbly3b.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "MÄR - Kaze to RAINBOW by GARNET CROW",
+    videoId : "https://naedist.animemusicquiz.com/s3irx5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Meitantei Conan: Tantei-tachi no Requiem - Yuruginai Mono Hitotsu by B'z",
+    videoId : "https://naedist.animemusicquiz.com/coty5t.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Memories Off #5 Togireta Film - Romancing Story by Ayane",
+    videoId : "https://naedist.animemusicquiz.com/zyouo7.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Nana1 - a little pain by OLIVIA inspi' REIRA(TRAPNEST)",
+    videoId : "https://naedist.animemusicquiz.com/iunkqy.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Nana2 - Wish by OLIVIA inspi' REIRA(TRAPNEST)",
+    videoId : "https://naedist.animemusicquiz.com/efli32.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Naruto1 - Yellow Moon by Akeboshi",
+    videoId : "https://naedist.animemusicquiz.com/gz3y55.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Naruto2 - Yura Yura by Hearts Grow",
+    videoId : "https://naedist.animemusicquiz.com/ohonxl.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Naruto3 - Parade by CHABA",
+    videoId : "https://naedist.animemusicquiz.com/oqlg4k.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "NHK ni Youkoso!1 - Puzzle by ROUND TABLE featuring Nino",
+    videoId : "https://naedist.animemusicquiz.com/erjzni.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "NHK ni Youkoso!2 - Modokashii Sekai no Ue de by Yui Makino",
+    videoId : "https://naedist.animemusicquiz.com/o7arh0.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Oban Star-Racers - Waratteta by Sukoshi",
+    videoId : "https://naedist.animemusicquiz.com/03pkrp.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "One Piece - BRAND NEW WORLD by D-51",
+    videoId : "https://naedist.animemusicquiz.com/aemf9t.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Shin Kyuuseishu Densetsu Hokuto no Ken: Raoh-den Junai no Shou - Pierrot by Aya Kamiki",
+    videoId : "https://naedist.animemusicquiz.com/v1fye5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Simoun - “Utsukushi Kereba Sore de Ii” by Chiaki Ishikawa",
+    videoId : "https://naedist.animemusicquiz.com/5qvb8q.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Sky Girls OVA - Baby's Tears by Riyu Kosaka",
+    videoId : "https://naedist.animemusicquiz.com/7cs1w6.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Spider Rider S1 ED2 - Koi no Keshiki by Tamaru Yamada",
+    videoId : "https://naedist.animemusicquiz.com/wsu3b4.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Suzumiya Haruhi no Yuuutsu - God knows... by Aya Hirano",
+    videoId : "https://naedist.animemusicquiz.com/mk1eey.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Tsuyokiss: Cool×Sweet - open by Kaori Utatsuki",
+    videoId : "https://naedist.animemusicquiz.com/xy6kyl.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Utawarerumono1 - Kimi ga Tame by Suara",
+    videoId : "https://naedist.animemusicquiz.com/34njnq.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Utawarerumono2 - Musouka by Suara",
+    videoId : "https://naedist.animemusicquiz.com/t5bzh7.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -250,8 +530,8 @@ export const songs = [
         ]
     }
 },{
-    name: "Spy × Family - uneven fruit by (K)NoW_NAME:NIKIIE",
-    videoId : "https://nl.catbox.video/07824z.webm",
+    name: "Zegapain - Kimi e Mukau Hikari by Akino Arai",
+    videoId : "https://naedist.animemusicquiz.com/bq5yb7.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
